300bd5059899a1e3fd556eb272c7e4a37a880e27
James <Jlassos@gmail.com>
2016-02-25 18:09:09 -0800

    first commit

---
+++ src/cashRegister.js
@@ -0,0 +1,24 @@
+const cashRegister = {
+    items: []
+}
+
+function addItem(register, name, cost) {
+    const item = {
+        name: name,
+        cost: cost
+    }
+
+    register.items.push(item)
+}
+
+function getTotal(register) {
+    return register.items.reduce((prev, curr) => prev.cost + curr.cost)
+}
+
+////////////////////////Usage
+
+addItem(cashRegister, 'shoes', 50)
+addItem(cashRegister, 'shirt', 15)
+
+const total = getTotal(cashRegister)
+console.log('Total is:', total)
